5a33c72847dc2d9f973dcf0ac2f4358b4045e33a
贺全喜 <hequanxi@bianfeng.com>
2020-11-30 10:49:56 +0800

    新增爬虫小例子

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/mesfreeman/study-go
 
 go 1.13
+
+require (
+	github.com/elazarl/goproxy v0.0.0-20201021153353-00ad82a08272 // indirect
+	github.com/parnurzeal/gorequest v0.2.16
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tidwall/gjson v1.6.3
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+	moul.io/http2curl v1.0.0 // indirect
+)
---
+++ spider/02capture2txt/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/parnurzeal/gorequest"
+	"github.com/tidwall/gjson"
+)
+
+// 采集搜狗哈哈段子
+func main() {
+	ch := make(chan int)
+
+	startPage := 1
+	endPage := 30
+
+	for i := startPage; i <= endPage; i++ {
+		go spider(i, ch)
+	}
+
+	for i := startPage; i <= endPage; i++ {
+		currentPage := <-ch
+		fmt.Println("Current page is " + strconv.Itoa(currentPage))
+	}
+}
+
+// 爬取页面
+func spider(page int, ch chan int) {
+	url := "http://m.haha.sogou.com/getMore/index?key=text&page=" + strconv.Itoa(page)
+
+	// 获取响应内容
+	_, body, errs := gorequest.New().Get(url).End()
+	if errs != nil {
+		fmt.Println("Get url fail.", errs)
+		os.Exit(0)
+	}
+
+	// 解析内容
+	//total := gjson.Parse(body).Get("total").Int()
+	//pagenum := gjson.Parse(body).Get("pagenum").Int()
+	texts := gjson.Parse(body).Get("list.#.text").Array()
+	for _, text := range texts {
+		//fmt.Println(text.String() + "\n")
+
+		fl, err := os.OpenFile("funny.txt", os.O_APPEND|os.O_CREATE, 0644)
+		if errs != nil {
+			fmt.Println("Open file fail.", err.Error())
+			os.Exit(0)
+		}
+
+		defer fl.Close()
+
+		bytes := []byte(text.String() + "\n\n")
+		fl.Write(bytes)
+	}
+
+	ch <- page
+}
