c85d31ba884e73d8e47a0c4fb006db888cf639c6
Double <double.he@qq.com>
2021-06-16 22:42:50 +0800

    arr sort

---
+++ shanggu/chapter08/arrsort/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// 冒泡排序
+func BubbleSort(arr *[5]int) *[5]int {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-1-i; j++ {
+			if (*arr)[j] > (*arr)[j+1] {
+				(*arr)[j], (*arr)[j+1] = (*arr)[j+1], (*arr)[j]
+			}
+		}
+	}
+	return arr
+}
+
+func main() {
+	var arr = [...]int{24, 69, 80, 57, 13}
+	fmt.Println(BubbleSort(&arr))
+}
