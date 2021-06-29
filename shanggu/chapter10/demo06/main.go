5482479a09b2c1466747951b357115fffcf4f037
贺全喜 <hequanxi@bianfeng.com>
2021-06-29 10:17:42 +0800

    struct test

--- shanggu/chapter10/demo06/main.go
+++ shanggu/chapter10/demo06/main.go
@@ -4,9 +4,8 @@ import "fmt"
 
 type MethodUtils01 struct{}
 
-type MethodUtils02 struct{}
-
-func (m *MethodUtils01) Print() {
+// 编程一个方法，方法不需要参数，在方法中打印一个 10.* 8 的矩形，在main方法中调用该方法。
+func (u *MethodUtils01) Print() {
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 8; j++ {
 			fmt.Print("* ")
@@ -15,6 +14,9 @@ func (m *MethodUtils01) Print() {
 	}
 }
 
+type MethodUtils02 struct{}
+
+// 编写一个方法，提供m和n两个参数，方法中打印一个m*n的矩形
 func (u *MethodUtils02) Print(m, n int) {
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
@@ -24,6 +26,15 @@ func (u *MethodUtils02) Print(m, n int) {
 	}
 }
 
+type MethodUtils03 struct {
+	Length int
+	Width  int
+}
+
+func (u *MethodUtils03) Area() int {
+	return u.Length * u.Width
+}
+
 func main() {
 	var m01 MethodUtils01
 	m01.Print()
@@ -31,4 +42,11 @@ func main() {
 	fmt.Println("-----------")
 	var m02 MethodUtils02
 	m02.Print(2, 3)
+
+	fmt.Println("-----------")
+	m03 := &MethodUtils03{
+		Length: 10,
+		Width:  2,
+	}
+	fmt.Println("area: ", m03.Area())
 }
