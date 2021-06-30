cd1f35f9f046aad76e62767ccae0c792581f5e83
贺全喜 <hequanxi@bianfeng.com>
2021-06-30 11:13:49 +0800

    study extends

---
+++ shanggu/chapter11/demo02/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// 继承初探
+type Student struct {
+	Name  string
+	Age   int
+	Score int
+}
+
+// 介绍
+func (s *Student) Say() {
+	fmt.Println("My name is " + s.Name)
+}
+
+// 小学生
+type Pupil struct {
+	Student
+}
+
+// 大学生
+type College struct {
+	Student
+}
+
+func main() {
+	pupic := &Pupil{}
+	pupic.Name = "xiaohe"
+	pupic.Say()
+
+	college := &College{}
+	college.Student.Name = "xiaohong"
+	college.Say()
+}
