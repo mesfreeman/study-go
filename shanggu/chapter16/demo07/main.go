f108fb04a5ef675d82c91d925ea4cb44e073db99
Double <double.he@qq.com>
2021-07-10 22:22:19 +0800

    study channel

---
+++ shanggu/chapter16/demo07/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func addNum(numChan chan int) {
+	for i := 1; i <= 200; i++ {
+		numChan <- i
+	}
+	close(numChan)
+}
+
+func readNum(numChan chan int, resChan chan map[int]int, exitChan chan bool) {
+	for {
+		var res int
+		num, ok := <-numChan
+		if !ok {
+			exitChan <- true
+			break
+		}
+
+		for i := 1; i <= num; i++ {
+			res += i
+		}
+
+		resChan <- map[int]int{num: res}
+	}
+}
+
+func main() {
+	numChan := make(chan int, 500)
+	go addNum(numChan)
+
+	resChan := make(chan map[int]int, 200)
+	exitChan := make(chan bool, 8)
+
+	starTime := time.Now()
+	for i := 0; i < 8; i++ {
+		go readNum(numChan, resChan, exitChan)
+	}
+
+	go func() {
+		for i := 0; i < 8; i++ {
+			<-exitChan
+		}
+		close(resChan)
+		close(exitChan)
+		fmt.Println("耗时：", time.Since(starTime).Nanoseconds())
+	}()
+
+	for val := range resChan {
+		fmt.Println(val)
+	}
+}
